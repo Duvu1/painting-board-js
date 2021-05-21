0efba5aff8fcd12e3245d22116c8b9d191a1ac69
Duvu1 <67996676+Duvu1@users.noreply.github.com>
2021-05-21 23:57:01 +0900

    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
-
 
 let painting = false;
 
@@ -27,8 +30,10 @@ function onMouseMove(event) {
     }
 } 
 
-function onMouseDown(event){
-    painting = true;
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if(canvas){
@@ -37,3 +42,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
